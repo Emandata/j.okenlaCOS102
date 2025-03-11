cc8b4d169a3d58e5713829893f7191f15e59cf09
Emmanuel Okenla <eokenla02@gmail.com>
2025-03-11 02:05:08 +0100

    (week 1)

---
+++ week_1/practice_1.py
@@ -0,0 +1,12 @@
+#Integer number
+num = 100
+print(num)
+print("Data Type of varaible num is", type(num))
+#float
+fnum = 34.45
+print(fnum)
+print("Data Type of varaible fnum is", type(fnum))
+#complex no
+cnum = 3 +4j
+print(cnum)
+print("Data Type of varaible cnum is", type(cnum))
---
+++ week_1/practice_2.py
@@ -0,0 +1,9 @@
+str1 = 'Hi my name is Mathew. I am String'
+str2 = 'Hi my name is Precious. I am also String'
+#display string str1 and its type
+print(str1)
+print(type(str1))
+
+#displaying string str2 and its type
+print(str2)
+print(type(str2))
